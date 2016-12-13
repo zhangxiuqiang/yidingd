4cc4954c8338c7e047ee5841f34dff55798ee7a3
huchuan <749298396@qq.com>
2016-12-13 10:55:21 +0800

    ssa

--- js/jianli.js
+++ js/jianli.js
@@ -1,94 +1,72 @@
-/**
- * Created by Administrator on 2016/8/9.
- */
+window.onload=function () {
 
-function orient() {
-    //alert('gete');
-    if (window.orientation == 0 || window.orientation == 180) {
-        $("body").attr("class", "portrait");
-        orientation = 'portrait';
-        return false;
+    function orient() {
+        //alert('gete');
+        if (window.orientation == 0 || window.orientation == 180) {
+            $("body").attr("class", "portrait");
+            orientation = 'portrait';
+            return false;
+        }
+        else if (window.orientation == 90 || window.orientation == -90) {
+            $("body").attr("class", "portrait");
+            orientation = 'portrait';
+
+            return false;
+        }
     }
-    else if (window.orientation == 90 || window.orientation == -90) {
-        $("body").attr("class", "portrait");
-        orientation = 'portrait';
-
-        return false;
-    }
-}
 
 
-$(function(){
-    orient();
-});
+    $(function () {
+        orient();
+    });
 
 
-$(window).bind( 'orientationchange', function(e){
-    orient();
-});
+    $(window).bind('orientationchange', function (e) {
+        orient();
+    });
 
 
-~function (desW) {
-    var winW = document.documentElement.clientWidth,
-        oMain = document.getElementById("main"),
-        n = winW / desW;
-    if (winW > desW) {
-        oMain.style.width = desW + "px";
-        return;
-    }
-    document.documentElement.style.fontSize = n * 100 + "px";
-}(640);
-
-//->Swiper
-var sw = new Swiper(".swiper-container", {
-    loop: true,
-    direction: "vertical",
-    /*--当切换结束的时候我需要给当前展示的这个区域增加对应的ID(page1或者page2),从而实现里面子元素的运动--*/
-    onSlideChangeEnd: function (swiper) {
-        //->获取总共的SLIDE
-        var slideAry = swiper.slides,
-            trueNum = slideAry.length - 2,
-            lastIn = trueNum + 1;
-
-        //->获取当前活动块的索引
-        var curIn = swiper.activeIndex;
-
-        [].forEach.call(slideAry, function (item, index) {
-            if (index == curIn) {
-                switch (curIn) {
-                    case 0:
-                        item.id = "page" + trueNum;
-                        break;
-                    case lastIn:
-                        item.id = "page1";
-                        break;
-                    default:
-                        item.id = "page" + curIn;
-                }
-                return;
-            }
-            item.id = null;
-        });
-    }
-});
-~function () {
-    var music = document.getElementById("music"),
-        musicAudio = document.getElementById("musicAudio");
-    music.addEventListener("click", function () {
-        if (musicAudio.paused) {//->当前是暂停的
-            musicAudio.play();
-            music.className = "music move";
+    ~function (desW) {
+        var winW = document.documentElement.clientWidth,
+            oMain = document.getElementById("main"),
+            n = winW / desW;
+        if (winW > desW) {
+            oMain.style.width = desW + "px";
             return;
         }
-        musicAudio.pause();
-        music.className = "music";
-    }, false);
+        document.documentElement.style.fontSize = n * 100 + "px";
+    }(640);
 
-    window.setTimeout(function () {
-        musicAudio.play();
-        musicAudio.addEventListener("canplay", function () {
-            music.style.display = "block";
-            music.className = "music move";
-        }, false);
-    }, 1000);
-}();
+//->Swiper
+    var sw = new Swiper(".swiper-container", {
+        loop: true,
+        direction: "vertical",
+        /*--当切换结束的时候我需要给当前展示的这个区域增加对应的ID(page1或者page2),从而实现里面子元素的运动--*/
+        onSlideChangeEnd: function (swiper) {
+            //->获取总共的SLIDE
+            var slideAry = swiper.slides,
+                trueNum = slideAry.length - 2,
+                lastIn = trueNum + 1;
+
+            //->获取当前活动块的索引
+            var curIn = swiper.activeIndex;
+
+            [].forEach.call(slideAry, function (item, index) {
+                if (index == curIn) {
+                    switch (curIn) {
+                        case 0:
+                            item.id = "page" + trueNum;
+                            break;
+                        case lastIn:
+                            item.id = "page1";
+                            break;
+                        default:
+                            item.id = "page" + curIn;
+                    }
+                    return;
+                }
+                item.id = null;
+            });
+        }
+    });
+}
